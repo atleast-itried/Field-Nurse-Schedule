c71f024d3853ee68efdf07d1cfb131d5cb7b49b6
atleast-itried <gonzalo.bas@gmail.com>
2025-04-17 16:18:31 +0200

    Fix: Add SSL configuration for Heroku PostgreSQL

--- packages/api/src/init.ts
+++ packages/api/src/init.ts
@@ -4,6 +4,9 @@ import { createTables, generateTimeSlots } from './models';
 export const initDb = async () => {
   const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   });
 
   try {
